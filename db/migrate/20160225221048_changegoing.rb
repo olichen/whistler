4ff5116006283e1527144b9da3fb2e9b1022bcb2
Oliver Chen <oliver.chen@navy.mil>
2016-02-25 17:28:39 -0500

    postgres fixing

--- db/migrate/20160225221048_changegoing.rb
+++ db/migrate/20160225221048_changegoing.rb
@@ -1,5 +1,5 @@
 class Changegoing < ActiveRecord::Migration
   def change
-    change_column :people, :going, :integer
+    change_column :people, :going, 'integer USING CAST(column_name AS integer)'
   end
 end
